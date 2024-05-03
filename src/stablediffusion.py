41602314f64f94bcd69700bb7f2c646c2e9157a4
Matt-Joshua Tan <mattjoshuatan@gmail.com>
2024-05-02 23:22:16 -0400

    stable diffusion just will not work on my macbook :(

--- src/stablediffusion.py
+++ src/stablediffusion.py
@@ -22,6 +22,9 @@ class StableDiffusion():
         '''
 
         # Load the pipeline
+
+        # I've been trying my hardest to get this to work on my Macbook M! 2020, but it doesn't. When I render the image, a floating point precision error occurs and the image returns as black. This doesn't happen on my Windows PC.
+        # The alternative solution is to set torch_dtype=torch.float32, but this is way too slow even after reducing the inference steps and dimensions.
         self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
         self.pipe = self.pipe.to(self.device)
         self.pipe.enable_attention_slicing() # Performance optimization for low VRAM
